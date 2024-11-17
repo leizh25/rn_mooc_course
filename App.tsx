db1c39c4d3cc5b585f370d33a1aa2ef10eb7bf2d
leizh25 <leizhuzheng@qq.com>
2024-11-17 22:50:00 +0800

    修复文件位置

--- App.tsx
+++ App.tsx
@@ -11,12 +11,12 @@ import StatusBarPage from './src/modules/statusBar';
 import SwitchPage from './src/modules/switch';
 import PersonalInfo from './src/modules/personalInfo';
 import Testapi from './src/modules/TestApi';
-import Anim1 from './android/app/src/anim/Anim1';
-import Anim2 from './android/app/src/anim/Anim2';
-import Anim3 from './android/app/src/anim/Anim3';
-import Anim4 from './android/app/src/anim/Anim4';
-import Anim5 from './android/app/src/anim/Anim5';
-import Anim6 from './android/app/src/anim/Anim6';
+import Anim1 from './src/anim/Anim1';
+import Anim2 from './src/anim/Anim2';
+import Anim3 from './src/anim/Anim3';
+import Anim4 from './src/anim/Anim4';
+import Anim5 from './src/anim/Anim5';
+import Anim6 from './src/anim/Anim6';
 const App = () => {
   return (
     <>
